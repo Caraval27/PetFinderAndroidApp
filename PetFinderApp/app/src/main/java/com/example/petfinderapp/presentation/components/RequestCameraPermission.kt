740339e759a01fc56afd1b03509e1f4226fb4a05
Caraval27 <bettypori@gmail.com>
2024-12-28 16:02:50 +0100

    Take picture with camera in post

---
+++ PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/components/RequestCameraPermission.kt
@@ -0,0 +1,37 @@
+package com.example.petfinderapp.presentation.components
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun RequestCameraPermission(
+    onPermissionGranted: () -> Unit,
+    onPermissionDenied: () -> Unit
+) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            onPermissionGranted()
+        } else {
+            onPermissionDenied()
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        val permissionCheck = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            onPermissionGranted()
+        } else {
+            permissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+}
