3ee37b7d5f9e347d9aedef24c2e374a010fc6d5a
Caraval27 <bettypori@gmail.com>
2024-12-28 13:23:19 +0100

    Create post and select photos from gallery.

--- PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/viewModel/PetFinderVM.kt
+++ PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/viewModel/PetFinderVM.kt
@@ -1,4 +1,39 @@
 package com.example.petfinderapp.presentation.viewModel
 
-class PetFinderVM {
+import androidx.lifecycle.ViewModel
+
+class PetFinderVM : ViewModel() {
+    private val posts = mutableListOf<Post>()
+
+    fun createPost(
+        title: String,
+        animalType: String,
+        race: String,
+        color: String,
+        username: String,
+        phoneNumber: String,
+        description: String,
+        postType: Boolean,
+        images: List<String>
+    ) {
+        posts.add(
+            Post(
+                title, animalType, race, color, username, phoneNumber, description, postType, images
+            )
+        )
+    }
+
+    fun getPosts(): List<Post> = posts
 }
+
+data class Post(
+    val title: String,
+    val animalType: String,
+    val race: String,
+    val color: String,
+    val username: String,
+    val phoneNumber: String,
+    val description: String,
+    val postType: Boolean, //true = found, false == looking
+    val images: List<String>
+)
