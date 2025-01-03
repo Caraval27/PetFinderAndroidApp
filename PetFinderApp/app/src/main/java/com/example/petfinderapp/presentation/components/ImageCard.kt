23f857c1b6eed13818a74860bb9ba0dfe769604e
Olivia Hultman <olivoljahultman@gmail.com>
2025-01-03 04:57:15 +0100

    Fixat en vy för ett visst inlägg, men behöver snyggas till

--- PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/components/ImageCard.kt
+++ PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/components/ImageCard.kt
@@ -1,6 +1,7 @@
 package com.example.petfinderapp.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
@@ -9,13 +10,14 @@ import androidx.compose.ui.layout.ContentScale
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun ImageCard(imageUri: String) {
+fun ImageCard(imageUri: String, onImageClick: () -> Unit) {
     Image(
         painter = rememberAsyncImagePainter(model = imageUri),
-        contentDescription = "Image from post",
+        contentDescription = "Post image",
         modifier = Modifier
             .aspectRatio(1f)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable(onClick = onImageClick),
         contentScale = ContentScale.Crop
     )
 }
