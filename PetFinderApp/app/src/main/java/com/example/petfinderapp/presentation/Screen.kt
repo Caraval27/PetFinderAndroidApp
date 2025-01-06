8ac6bc27de06a87f2a4c47c589bd119fa6f7ebfa
Caraval27 <bettypori@gmail.com>
2025-01-06 22:24:49 +0100

    changed found icon

--- PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/Screen.kt
+++ PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/Screen.kt
@@ -2,15 +2,11 @@ package com.example.petfinderapp.presentation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.petfinderapp.R
 
-sealed class Screen(val route: String, val icon: ImageVector, val label: String) {
-    data object Found : Screen("found", Icons.Filled.Check, "Found")
+sealed class Screen(val route: String, val icon: Any, val label: String) {
+    data object Found : Screen("found", R.drawable.found_icon, "Found")
     data object Looking : Screen("looking", Icons.Filled.Search, "Looking")
     data object CreatePost : Screen("post", Icons.Default.Add, "Post")
 }
