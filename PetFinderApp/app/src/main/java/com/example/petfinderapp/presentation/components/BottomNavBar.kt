516162b3e64c80e79d83e66127eed9a757e49214
Olivia Hultman <olivoljahultman@gmail.com>
2025-01-08 00:03:57 +0100

    Listan filtreras endast på datum om inget filter valt

--- PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/components/BottomNavBar.kt
+++ PetFinderApp/app/src/main/java/com/example/petfinderapp/presentation/components/BottomNavBar.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.example.petfinderapp.presentation.Screen
+import com.example.petfinderapp.presentation.utils.Screen
 
 @Composable
 fun BottomNavBar(navController: NavController) {
